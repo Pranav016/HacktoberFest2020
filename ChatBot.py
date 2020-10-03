7c3f2f9fb8472bbdbe43a9800cabdd4017d9c3d2
Jasmeet Singh <jasmeets199786@gmail.com>
2020-10-03 17:07:32 +0530

    Create ChatBot.py

---
+++ ChatBot.py
@@ -0,0 +1,16 @@
+x = input("  ")
+x = x.lower()
+if(x == "hi" or x == "hello" or x == ""):
+    print(" Hi Sir/Mam, How are you ?")
+    x = input("  ")
+    x = x.lower()
+    if(x == "I am fine" or "I am Fine"  or "you?" or "You?"):
+        print("I am also Fine. How may I help you?")
+        x = input("  ")
+        x = x.lower()
+        if(x == "Hacking" or "Cyber Security"):
+            print("REF: https://www.cybrary.it")
+            x = input("  ")
+            x = x.lower()
+            if(x == "blog" or x == "article" or x == tutorial):
+                print("https://www.guru99.com/ethical-hacking-tutorials.html")
